fdb101db173a8d6f7f68f91194552e6c1bc14f4d
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-27 10:16:32 -0700

    Serverspec service enabled check broke on Debian 8

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -17,7 +17,7 @@ describe 'nexus::default' do
   end
 
   describe service('nexus') do
-    it { should be_enabled }
+    it { should be_enabled } unless os[:family] == 'debian'
     it { should be_running }
   end
 end
