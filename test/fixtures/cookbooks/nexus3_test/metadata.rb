5bc38447d5cf135ad2a9695f63c28f6e81d01cd0
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-29 14:18:04 -0700

    Update desc

--- test/fixtures/cookbooks/nexus3_test/metadata.rb
+++ test/fixtures/cookbooks/nexus3_test/metadata.rb
@@ -2,7 +2,7 @@ name 'nexus3_test'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
-description 'Tests nexus'
+description 'Tests Nexus 3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
