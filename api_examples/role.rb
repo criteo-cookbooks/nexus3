da17c1a9be951136c23453ba631227b35b088a18
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-15 14:37:00 -0700

    Fix typo

--- api_examples/role.rb
+++ api_examples/role.rb
@@ -1,7 +1,7 @@
 # add role
 nexus3_api 'role-developer' do
   content "security.addRole('developer', 'Developer'," \
-    " 'User with privileges to allow read access to repo content and healtcheck'," \
+    " 'User with privileges to allow read access to repo content and healthcheck'," \
     " ['nx-healthcheck-read', 'nx-healthcheck-summary-read'], ['nx-anonymous'])"
   action :run
 end
