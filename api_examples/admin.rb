289bffd9aee19c0329de218ac79a5f1a3104d517
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-15 10:17:13 -0700

    Update admin example

--- api_examples/admin.rb
+++ api_examples/admin.rb
@@ -1,18 +1,18 @@
-# change admin password
+# Change admin password.  Note that the action is set to nothing.  This should
+# only be ran once during initial install of Nexus, otherwise, you will get a
+# 'wait up to 600 seconds for Nexus Rest API endpoint to respond' exception
+# because the default password 'admin123' will be incorrect.
 nexus3_api 'admin_change_password' do
-  context 'security.securitySystem.changePassword("admin", args[0])'
+  content "security.securitySystem.changePassword('admin', args)"
   args 'admin456'
-  action :run
+  action :nothing
 end
 
-nexus3_api 'admin_change_password' do
+# Update admin's email address using the new admin password
+nexus3_api 'admin_update_email' do
+  content "def user = security.securitySystem.getUser('admin');" \
+    "user.setEmailAddress('admin@mycompany.com');" \
+    "security.securitySystem.updateUser(user);"
   password 'admin456'
-  context 'security.securitySystem.changePassword("admin", args[0])'
-  action :run
-end
-
-nexus3_api 'admin_change_password' do
-  context 'security.securitySystem.changePassword("admin", args[0])'
-  args 'admin456'
   action :run
 end
