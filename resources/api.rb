77f04e2a5446e30be6a9b3926540c304bfe1fc1e
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-19 10:58:17 -0700

    Fix #10 nexus3_api ignore_failure attribute defaults to false

--- resources/api.rb
+++ resources/api.rb
@@ -5,19 +5,26 @@ attribute :script_name, kind_of: String, name_attribute: true
 attribute :username, kind_of: String, default: 'admin'
 attribute :password, kind_of: String, default: 'admin123'
 attribute :content, kind_of: [String, NilClass]
-attribute :script_cookbook, kind_of: String, default: lazy { node['nexus3']['api']['script_cookbook'] }
+attribute :script_cookbook, kind_of: String
 attribute :script_source, kind_of: [String, NilClass]
 attribute :args, kind_of: [Array, String, NilClass]
-attribute :type, kind_of: String, default: lazy { node['nexus3']['api']['type'] }
-attribute :host, kind_of: String, default: lazy { node['nexus3']['api']['host'] }
-attribute :endpoint, kind_of: String, default: lazy { node['nexus3']['api']['endpoint'] }
-attribute :ignore_failure, kind_of: [TrueClass, FalseClass], default: lazy { node['nexus3']['api']['ignore_failure'] }
-attribute :live_stream, kind_of: [TrueClass, FalseClass], default: lazy { node['nexus3']['api']['live_stream'] }
-attribute :wait, kind_of: Integer, default: lazy { node['nexus3']['api']['wait'] }
+attribute :type, kind_of: String
+attribute :host, kind_of: String
+attribute :endpoint, kind_of: String
+attribute :ignore_failure, kind_of: [TrueClass, FalseClass]
+attribute :live_stream, kind_of: [TrueClass, FalseClass]
+attribute :wait, kind_of: Integer
 attribute :sensitive, kind_of: [TrueClass, FalseClass] # , default: true - see initialize below
 
-# Chef will override sensitive back to its global value, so set default to true in init
 def initialize(*args)
   super
+  @script_cookbook = lazy { node['nexus3']['api']['script_cookbook'] }
+  @type = lazy { node['nexus3']['api']['type'] }
+  @host = lazy { node['nexus3']['api']['host'] }
+  @endpoint = lazy { node['nexus3']['api']['endpoint'] }
+  @ignore_failure = lazy { node['nexus3']['api']['ignore_failure'] }
+  @live_stream = lazy { node['nexus3']['api']['live_stream'] }
+  @wait = lazy { node['nexus3']['api']['wait'] }
+  # Chef will override sensitive back to its global value, so set default to true in init
   @sensitive = lazy { node['nexus3']['api']['sensitive'] }
 end
