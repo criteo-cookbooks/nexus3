6280f8e754e9a738e4aa7af91911d1aabe33e9cb
Olivier Tharan <o.tharan@criteo.com>
2017-05-24 10:50:57 +0200

    Use WebMock to stop outgoing connections in unittests
    Also stop retrying connecting in rspec with a specific attibute.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,20 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'webmock/rspec'
 
-WebMock.allow_net_connect!
-
 VER = '3.3.1-01'.freeze
 CACHE = Chef::Config[:file_cache_path]
 
 def json_response(result)
   JSON.generate(result)
+end
+
+RSpec.configure do |config|
+  # Disable all http requests
+  WebMock.disable_net_connect!(allow_localhost: true, allow: /supermarket.chef.io/i)
+  config.before(:each) do
+    # Reset http stubs
+    WebMock.reset!
+  end
 end
 
 ChefSpec::Coverage.start!
