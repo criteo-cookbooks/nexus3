c31e00aacec6029992e32689fd945f333ef5a5b6
Olivier Tharan <o.tharan@criteo.com>
2017-05-29 15:28:42 +0200

    Add a missing trailing slash to base_url

--- attributes/api.rb
+++ attributes/api.rb
@@ -1,7 +1,7 @@
 default['nexus3']['api']['script_cookbook'] = 'nexus3'
 default['nexus3']['api']['type'] = 'groovy'
 default['nexus3']['api']['host'] = 'http://localhost:8081'
-default['nexus3']['api']['endpoint'] = "#{node['nexus3']['api']['host']}/service/siesta/rest/v1/script"
+default['nexus3']['api']['endpoint'] = "#{node['nexus3']['api']['host']}/service/siesta/rest/v1/script/"
 default['nexus3']['api']['ignore_failure'] = true
 default['nexus3']['api']['live_stream'] = false
 default['nexus3']['api']['wait'] = 600 # 10 minutes
