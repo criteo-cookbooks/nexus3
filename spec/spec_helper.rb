e656ce4576213b2b2485e8499b13f7e75415c7c7
Olivier Tharan <o.tharan@criteo.com>
2017-06-06 15:57:26 +0200

    Remove useless put_ark resource matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'webmock/rspec'
 
-require 'support/matchers'
-
 VER = '3.3.1-01'.freeze
 CACHE = Chef::Config[:file_cache_path]
 
--- spec/support/matchers.rb
+++
@@ -1,3 +0,0 @@
-def put_ark(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:ark, :put, resource_name)
-end
