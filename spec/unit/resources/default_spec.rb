01ef0e3061a6dbc80812476f12b2eebdb3cfde8c
Olivier Tharan <o.tharan@criteo.com>
2017-07-19 15:32:37 +0200

    Move recipe tests for Windows
    They ought to move from the recipe to the resource tests. Fixed some path
    issues as well.

--- spec/unit/resources/default_spec.rb
+++ spec/unit/resources/default_spec.rb
@@ -72,8 +72,70 @@ describe 'nexus3_test::default' do
 
   context 'windows' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'windows', version: '2012r2', step_into: 'nexus3',
+      ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2', step_into: 'nexus3',
                                file_cache_path: CACHE).converge(described_recipe)
+    end
+
+    it 'installs Nexus3' do
+      expect(chef_run).to install_nexus3('foo')
+    end
+
+    it 'creates nexus group' do
+      expect(chef_run).to create_group('Administrators')
+    end
+
+    it 'creates nexus user' do
+      expect(chef_run).to create_user('nexus')
+    end
+
+    it 'creates install dir' do
+      expect(chef_run).to create_directory("C:\\Nexus3/nexus-#{VER}")
+    end
+
+    it 'creates data dir' do
+      expect(chef_run).to create_directory('C:\Nexus3/sonatype-work/nexus3')
+ end
+
+    it 'creates etc dir' do
+      expect(chef_run).to create_directory('C:\Nexus3/sonatype-work/nexus3/etc')
+    end
+
+    it 'extracts nexus tarball' do
+      expect(chef_run).to put_ark("nexus-#{VER}")
+    end
+
+    it 'creates rc template' do
+      expect(chef_run).to create_template("C:\\Nexus3/nexus-#{VER}/bin/nexus.rc")
+      expect(chef_run.template("C:\\Nexus3/nexus-#{VER}/bin/nexus.rc"))
+        .to notify('log[foo is restarting]').delayed
+    end
+
+    it 'creates vmoptions template' do
+      expect(chef_run).to create_template("C:\\Nexus3/nexus-#{VER}/bin/nexus.vmoptions")
+      expect(chef_run.template("C:\\Nexus3/nexus-#{VER}/bin/nexus.vmoptions"))
+        .to notify('log[foo is restarting]').delayed
+    end
+
+    it 'creates properties template' do
+      expect(chef_run).to create_template('C:\Nexus3/sonatype-work/nexus3/etc/nexus.properties')
+      expect(chef_run.template('C:\Nexus3/sonatype-work/nexus3/etc/nexus.properties'))
+        .to notify('log[foo is restarting]').delayed
+    end
+
+    it 'creates homedir symlink' do
+      expect(chef_run).to create_link('C:\Nexus3/nexus3')
+    end
+
+    it 'enables nexus service' do
+      expect(chef_run).to enable_nexus3_service('foo')
+    end
+
+    it 'logs a service restart' do
+      expect(chef_run.log('foo is restarting')).to do_nothing
+    end
+
+    it 'blocks until operational does nothing' do
+      expect(chef_run.ruby_block('block until operational')).to do_nothing
     end
   end
 end
