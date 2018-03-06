0f80d037222bc51e7c5bbfd208c05430eb5a9e67
Baptiste Courtois <b.courtois@criteo.com>
2018-03-07 00:01:24 +0100

    Make nexus3_tasks integration tests compatible with 3.8.0

--- test/integration/default/serverspec/tasks_spec.rb
+++ test/integration/default/serverspec/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'serverspec_helper'
 
 describe 'nexus::tasks' do
-  describe command('curl -uadmin:admin123 http://localhost:8081/service/siesta/rest/v1/script/get_task/run -X POST ' \
+  describe command('curl -uadmin:admin123 http://localhost:8081/service/rest/v1/script/get_task/run -X POST ' \
                    '-H "Content-Type: text/plain" -d foo') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should match(/result.*name.*foo/) }
