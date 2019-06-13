2075520b8010ebb419bdb7f1235cb4ab87938a4b
Jeremy Clerc <j.clerc@criteo.com>
2019-06-13 09:30:00 +0200

    Replace Oracle Java by OpenJDK
    Every once in a while, the download of Java is broken,
    this time it is due to Java requiring to sign-in before
    downloading the sdk.
    
    NOTE: This change also reduces the test matrix to be able to
          use openjdk. New versions of debian and ubuntu are not
          added as their image does not contain systemd, this
          require some work to find other images, or build ours.
          To be eventually done in the future.

--- test/fixtures/cookbooks/nexus3_test/attributes/default.rb
+++ test/fixtures/cookbooks/nexus3_test/attributes/default.rb
@@ -1,3 +1,6 @@
 default['nexus3']['api']['username'] = 'admin'
 default['nexus3']['api']['password'] = 'admin123'
 default['nexus3']['api']['endpoint'] = 'http://localhost:8081/service/rest/v1/script/'
+
+default['java']['install_flavor'] = 'openjdk'
+default['java']['jdk_version'] = '8'
