1f808a8c4cc7c5ca778c8107fbb99424274c0f3e
Jeremy Clerc <j.clerc@criteo.com>
2019-08-26 11:04:24 +0200

    Bump to latest version 3.19.1, includes security fix
    * Get rid of specific change_password suite as we
      have to change password in any case due to 3.17
      generating a random admin password.
    * Update change_admin_spec to match test workflow

--- spec/unit/change_admin_password_spec.rb
+++ spec/unit/change_admin_password_spec.rb
@@ -1,57 +1,78 @@
 require 'spec_helper'
+require 'fakefs/safe'
 
 describe 'nexus3_test::change_admin_password' do
-  context 'linux' do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'centos',
-                               version: CENTOS_VERSION,
-                               step_into: 'nexus3',
-                               file_cache_path: CACHE)
-                          .converge('nexus3_test::default', 'nexus3_test::change_admin_password')
+  shared_examples 'update_admin_credentials' do
+    it 'reads generated credentials' do
+      expect(chef_run).to run_ruby_block('read generated admin password')
+      expect(chef_run.node['nexus3']['api']['password_wanted']).to be('newPassword')
+      expect(chef_run.node['nexus3']['api']['password_generated']).to be('S3cr3t')
     end
 
     it 'set admin credentials' do
       resource = chef_run.nexus3_api('admin_change_password')
-      expect(resource).to subscribe_to('nexus3[foo]').on(:create).immediately
-      expect(resource).to subscribe_to('nexus3[foo]').on(:run).immediately
-      expect(resource).to subscribe_to('nexus3[foo]').on(:delete).immediately
 
       expect(resource.script_name).to eq 'admin_change_password'
       expect(resource.content).to eq "security.securitySystem.changePassword('admin', args)"
       expect(resource.args).to eq 'newPassword'
 
-      expect(resource).to do_nothing
+      expect(chef_run).to create_nexus3_api('admin_change_password')
+      expect(chef_run).to run_nexus3_api('admin_change_password')
+      expect(chef_run).to delete_nexus3_api('admin_change_password')
     end
 
     it 'Update Nexus Credentials' do
       expect(chef_run).to run_ruby_block('update nexus api credentials for future resources')
+    end
+
+    it 'creates guard file' do
+      expect(chef_run).to create_file(::File.join(node['nexus3']['data'], 'admin.password.updated'))
+    end
+  end
+
+  context 'linux' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'centos',
+                                        version: CENTOS_VERSION,
+                                        step_into: 'nexus3',
+                                        file_cache_path: CACHE) do |node, _|
+        node.normal['nexus3']['data'] = '/opt/nexus-data'
+        node.normal['nexus3']['api']['password'] = 'newPassword'
+      end
+      runner.converge('nexus3_test::change_admin_password')
+    end
+
+    before do
+      ::FakeFS.activate!
+      ::FileUtils.mkdir_p('/opt/nexus-data')
+      ::File.write('/opt/nexus-data/admin.password', 'S3cr3t')
+    end
+
+    after do
+      ::FakeFS.deactivate!
     end
   end
 
   context 'windows' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'windows',
-                               version: '2012R2',
-                               step_into: 'nexus3',
-                               file_cache_path: CACHE)
-                          .converge('nexus3_test::default', 'nexus3_test::change_admin_password')
+      runner = ChefSpec::SoloRunner.new(platform: 'windows',
+                                        version: '2012R2',
+                                        step_into: 'nexus3',
+                                        file_cache_path: CACHE) do |node, _|
+        node.normal['nexus3']['data'] = 'C:\nexus-data'
+        node.normal['nexus3']['api']['password'] = 'newPassword'
+      end
+      runner.converge('nexus3_test::change_admin_password')
     end
 
-    it 'set admin credentials' do
-      resource = chef_run.nexus3_api('admin_change_password')
-      expect(resource).to subscribe_to('nexus3[foo]').on(:create).immediately
-      expect(resource).to subscribe_to('nexus3[foo]').on(:run).immediately
-      expect(resource).to subscribe_to('nexus3[foo]').on(:delete).immediately
-
-      expect(resource.script_name).to eq 'admin_change_password'
-      expect(resource.content).to eq "security.securitySystem.changePassword('admin', args)"
-      expect(resource.args).to eq 'newPassword'
-
-      expect(resource).to do_nothing
+    before do
+      ::FakeFS.activate!
+      ::FileUtils.mkdir_p('C:\nexus-data')
+      ::File.write('C:\nexus-data\admin.password', 'S3cr3t')
     end
 
-    it 'Update Nexus Credentials' do
-      expect(chef_run).to run_ruby_block('update nexus api credentials for future resources')
+    after do
+      ::FakeFS.deactivate!
     end
   end
 end
--- test/integration/change_password/serverspec/tasks_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'serverspec_helper'
-
-describe 'nexus::tasks' do
-  describe command('curl -uadmin:newPassword http://localhost:8081/service/rest/v1/script/get_task/run -X POST ' \
-                   '-H "Content-Type: text/plain" -d foo') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/result.*name.*foo/) }
-  end
-end
