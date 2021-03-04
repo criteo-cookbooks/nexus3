2d157e3bc836133e510aacf32ebe2071ba5e086a
Baptiste Courtois <b.courtois@criteo.com>
2021-03-04 19:57:43 +0100

    Coerce repositories attributes to ensure proper comparison
    The returned cleanup policies order may not be consistent.
    As the order is not important here, we can sort the array to ensure the
    resource is comparing the value properly and in a consistent way.

--- libraries/nexus_helper.rb
+++ libraries/nexus_helper.rb
@@ -4,6 +4,17 @@ require 'net/http'
 module Nexus3
   # Helper library for testing Nexus3 responses
   module Helper
+    # Apply some logic on repo attributes hash
+    def self.coerce_repo_attributes(value)
+      value = value.dup
+      # Ensure cleanup policies are sorted if any, to ensure coherent comparison
+      if value.dig('cleanup', 'policyName')
+        value['cleanup'] = value['cleanup'].dup
+        value['cleanup']['policyName'] = value['cleanup']['policyName'].sort
+      end
+      value
+    end
+
     # Raise an error in case Nexus takes a really long time to start.
     class Nexus3NotReady < StandardError
       def initialize(endpoint, timeout)
