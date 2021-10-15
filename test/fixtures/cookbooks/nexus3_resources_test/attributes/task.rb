80d0907c528fc244fd121881b537b0b356cdcb05
Jeremy Clerc <j.clerc@criteo.com>
2021-10-15 17:14:28 +0200

    task: test blobstore.compact

--- test/fixtures/cookbooks/nexus3_resources_test/attributes/task.rb
+++ test/fixtures/cookbooks/nexus3_resources_test/attributes/task.rb
@@ -3,11 +3,16 @@
 default['nexus3_resources_test']['task']['create']['default']['task_name'] = 'bask'
 default['nexus3_resources_test']['task']['create']['default']['task_type'] = 'script'
 
+default['nexus3_resources_test']['task']['create']['compact']['task_name'] = 'compact_default'
+default['nexus3_resources_test']['task']['create']['compact']['task_type'] = 'blobstore.compact'
+default['nexus3_resources_test']['task']['create']['compact']['crontab'] = '0 15 4 * * ?'
+default['nexus3_resources_test']['task']['create']['compact']['properties'] = { 'blobstoreName' => 'default' }
+
 # Test task update
 default['nexus3_resources_test']['task']['create']['update_with_properties']['task_name'] = 'bask'
 default['nexus3_resources_test']['task']['create']['update_with_properties']['task_type'] = 'script'
 default['nexus3_resources_test']['task']['create']['update_with_properties']['crontab'] = '0 2 * * * ?'
-default['nexus3_resources_test']['task']['create']['update_with_properties']['properties'] = {'language' => 'lang', 'source' => 'log.info("Hello task");'}
+default['nexus3_resources_test']['task']['create']['update_with_properties']['properties'] = { 'language' => 'lang', 'source' => 'log.info("Hello task");' }
 
 # action :delete
 default['nexus3_resources_test']['task']['delete']['default']['task_name'] = 'bask'
