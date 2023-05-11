27ddc9326cb04a8267f407c44dad8ea5686ca9b0
b.courtois <b.courtois@criteo.com>
2023-05-12 01:00:45 +0200

    Specify correct CentOS Fauxhai platform version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'webmock/rspec'
 
 VER = '3.27.0-03'.freeze
 CACHE = Chef::Config[:file_cache_path]
-CENTOS_VERSION = '7.8'.freeze
+CENTOS_VERSION = '7.8.2003'.freeze
 
 def api_response(code, result = {})
   { status: code, headers: { 'Content-Type' => 'application/json' }, body: JSON.generate(result) }
