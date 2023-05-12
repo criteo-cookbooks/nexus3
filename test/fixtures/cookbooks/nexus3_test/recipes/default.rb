15385856e3a552f36e3423e5fd9e4a77a58a1960
b.courtois <b.courtois@criteo.com>
2023-05-12 23:10:04 +0200

    Use ubuntu 20.04 instead of 16.04 in tests
    Unbuntu 16.04 is EOL since April 2021 and 18.04 since April 2023.
    Let's use the 20.04 which is the "oldest" current LTS.
    
    Also systemd version distributed with Ubuntu 16.04 was too old.
    This raised some bugs recently in Github Actions kitchen dokken tests.

--- test/fixtures/cookbooks/nexus3_test/recipes/default.rb
+++ test/fixtures/cookbooks/nexus3_test/recipes/default.rb
@@ -8,7 +8,7 @@ nexus3 'foo'
 # Second installation on the same machine with altered settings (not
 # tested on Windows).
 unless platform_family?('windows')
-  package 'iproute' # inspec tests on fedora-29 need this for `ss`
+  package 'iproute' if platform?('fedora') # inspec tests on fedora-29 need this for `ss`
   nexus3 'bar' do
     path '/usr/local/nexusbar/path'
     data '/usr/local/nexusbar/data'
