50ae1584b4e3d3c8381352a3daedce7c5fc9abcb
a.chong <a.chong@criteo.com>
2024-04-04 11:32:51 +0200

    Add property to configure logback.xml
    * Users are already able to configure logback for custom logging options
      outside of the cookbook. The main drawback is the restart of nexus
      required to take the changes into account.
    * Here we allow them to pass their options directly to the resource with
      a template file and the cookbook name, to apply the template in the
      same resource and notify the restart at the same time.
    
    Change-Id: If22eb60d0797487625c2b2f28b37d0897867ca3e

--- resources/default.rb
+++ resources/default.rb
@@ -18,6 +18,7 @@ property :properties_variables, Hash, default: lazy { node['nexus3']['properties
 property :vmoptions_variables, Hash, default: lazy { node['nexus3']['vmoptions_variables'] }
 property :outbound_proxy, [Hash, NilClass], sensitive: true, default: lazy { node['nexus3']['outbound_proxy'] }
 property :plugins, Hash, default: lazy { node['nexus3']['plugins'] }
+property :logback_variables, Hash, default: lazy { node['nexus3']['logback_variables'] }
 
 action :install do
   install_dir = ::File.join(new_resource.path, "nexus-#{new_resource.version}")
@@ -106,6 +107,17 @@ action :install do
     group new_resource.nexus3_group
     notifies :restart, "nexus3_service[#{new_resource.service_name}]", :delayed
     notifies :run, "ruby_block[#{blocker}]", :delayed
+  end
+
+  template ::File.join(new_resource.data, 'etc', 'logback', 'logback.xml') do
+    source new_resource.logback_variables['source']
+    cookbook new_resource.logback_variables['cookbook']
+    mode '0644'
+    owner new_resource.nexus3_user
+    group new_resource.nexus3_group
+    notifies :restart, "nexus3_service[#{new_resource.service_name}]", :delayed
+    notifies :run, "ruby_block[#{blocker}]", :delayed
+    not_if { new_resource.logback_variables.empty? }
   end
 
   # Install plugins
